94a8b48d617c849495df0c20100e06bac7be1bea
Sadeem Khan <imsadeem98@gmail.com>
2020-03-25 18:55:43 -0700

    Finished

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let colors = [];
 let numSquares = 6;
 let pickedColor;
+
 let squares = document.querySelectorAll(".square");
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1");
 let resetButton = document.querySelector("#reset");
 let modeButtons = document.querySelectorAll(".mode");
-
 
 init();
 
@@ -20,6 +20,18 @@ function init(){
 
 	//reset
 	reset();
+}
+
+function setModeButtonListeners(){
+	for (let i = 0; i < modeButtons.length; i++){
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
+			reset();
+		});
+	}
 }
 
 function setUpSquares(){
@@ -70,18 +82,6 @@ function reset(){
 resetButton.addEventListener("click", function(){
 	reset();
 });
-
-function setModeButtonListeners(){
-	for (let i = 0; i < modeButtons.length; i++){
-		modeButtons[i].addEventListener("click", function(){
-			modeButtons[0].classList.remove("selected");
-			modeButtons[1].classList.remove("selected");
-			this.classList.add("selected");
-			this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
-			reset();
-		});
-	}
-}
 
 function changeColors(color) {
 	for (let i = 0; i < squares.length; i++){
